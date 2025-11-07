e32d6e29ccc59ee054469be6a0f6add20839363b
yvuzs <81232554+yvuzs@users.noreply.github.com>
2025-11-08 00:58:27 +0300

    Refactor service worker for improved caching

--- sw.js
+++ sw.js
@@ -1,29 +1,35 @@
-<!-- sw.js (Service Worker) -->
-<script type="text/plain" id="__pwa_sw">
+// /gorev-takip/sw.js
 const CACHE_NAME = 'gorev-takip-v3';
 const ASSETS = [
-'./',
-'./index.html', // kendi dosya adını yaz: gorev_takip.html gibi
-'./manifest.webmanifest',
-'./icons/icon-192.png',
-'./icons/icon-512.png'
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
 ];
-self.addEventListener('install', e=>{
-e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));
-});
-self.addEventListener('activate', e=>{
-e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))));
-self.clients.claim();
-});
-self.addEventListener('fetch', e=>{
-e.respondWith(
-caches.match(e.request).then(r=> r || fetch(e.request).then(res=>{
-const copy = res.clone();
-caches.open(CACHE_NAME).then(c=>{ if(e.request.method==='GET' && res.ok) c.put(e.request, copy); });
-return res;
-}).catch(()=> caches.match('./')))
-);
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
 });
 
-</script>
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
 
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        const clone = res.clone();
+        caches.open(CACHE_NAME).then(c => c.put(e.request, clone));
+        return res;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
